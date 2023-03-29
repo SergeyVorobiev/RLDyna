1b90bd4508f26dd44ed231a371ede5f1930d3130
SergeyVorobiev <fxeipi@gmail.com>
2023-03-29 21:54:55 +0300

    Adjusted table borders for Cart Pole.

--- rl/agents/cartpole/CPTabularQAgent.py
+++ rl/agents/cartpole/CPTabularQAgent.py
@@ -19,7 +19,7 @@ class CPTabularQAgent(RDynaAgentBuilder):
     def build_agent(self, env: Env):
 
         # To avoid infinite reward growing
-        discount = 0.95
+        discount = 0.99
 
         # For constant Reward alpha less than 1 only slow you down because average of [a, a, a, a, a] will always be a.
         # But here we use discrete table meaning that potentially different states will be stored in the same table cell
@@ -31,10 +31,10 @@ class CPTabularQAgent(RDynaAgentBuilder):
         # Iterative algorithm
         algorithm = Q(e_greedy, alpha=alpha, discount=discount)
 
-        dim_min_max = [[-4, 4],
-                       [-4, 4],
-                       [-4, 4],
-                       [-4, 4]]
+        dim_min_max = [[-1.5, 1.5],
+                       [-3, 3],
+                       [-0.5, 0.5],
+                       [-3, 3]]
         table = DiscreteQTable(dim_min_max=dim_min_max, quant_size=40, n_actions=env.action_space.n,
                                model_path=self._model_path, load_model=self._load_model)
 
